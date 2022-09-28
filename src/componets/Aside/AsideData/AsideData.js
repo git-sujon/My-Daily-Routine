a9cd75fc7c28a963f6b4c64a3da08d05d0463d93
Mr-Robot-101 <git.sujon@gmail.com>
2022-09-29 01:13:18 +0600

    Font Awesome Install

--- src/componets/Aside/AsideData/AsideData.js
+++ src/componets/Aside/AsideData/AsideData.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import avatar from '../../../avatar.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const AsideData = () => {
     return (
         <div>
             <div className='profile d-flex justify-content-center align-items-center'>
-                <div className='avatar border border-5'>
-                    <img src={avatar} className='w-25 rounded-circle' />
-                </div>
                 <div>
+                    
                     <h3 className='fw-bold'>Mr Sujon</h3>
                 </div>
             </div>
